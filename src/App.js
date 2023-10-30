466559655870ee633fea715cb79d2e32cdd53a5e
Lior <audiblemaple@gmail.com>
2023-10-30 22:57:22 +0200

    Backup & initial commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import "./styles/Buttons_style.css"
-import HotspotButton from "./Components/Button";
+import './styles/App.css';
+import Banner from './Components/Banner';
+import ModelViewer from './Components/ModelViewer'
+import VideoPlayer from './Components/VideoPlayer';
 
+import "./styles/general_style.css"
+import PerspectiveButton from '../src/Components/PerspectiveButton';
 function App() {
-
-    fetch('https://ajax.googleapis.com/ajax/libs/model-viewer/3.3.0/model-viewer.min.js')
-        .then(response => response.text())  // Get the text response first
-        .then(text => {
-            console.log(text);  // Log the response to see what you're getting
-            return JSON.parse(text);
-        })
-        .catch(error => console.error(error));
-  return (
-    <model-viewer
-        id="model"
-        src="poly.glb"
-        camera-controls
-        shadow-intensity="1"
-        exposure="0.29"
-        shadow-softness="1"
-        interaction-prompt="none"
-        camera-orbit="0deg 60deg 30m"
-        max-camera-orbit="30deg 60deg 40m"
-        max-field-of-view="30deg"
-        min-camera-orbit="0deg 0deg 10m"
-        min-field-of-view="30deg"
-    >
-
-        <HotspotButton
-            slot="hotspot-2"
-            position="-4.4m -0.24m -4.2m"
-            normal="0m 0m 1m"
-            visibilityAttribute="visible"
-            blindOrLight="blind"
-        />
-    </model-viewer>
-
-  );
+    return (
+        <div className="container">
+            <div className="box">
+                <ModelViewer />
+            </div>
+            <Banner text="Smart home" />
+            <PerspectiveButton />
+        </div>
+    );
 }
 
 export default App;
