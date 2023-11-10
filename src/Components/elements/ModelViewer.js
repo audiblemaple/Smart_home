5630244f20a3534e1c21465c53f9420b30a554bd
Lior <audiblemaple@gmail.com>
2023-11-10 21:43:39 +0200

    removed static files

--- src/Components/elements/ModelViewer.js
+++ src/Components/elements/ModelViewer.js
@@ -2,6 +2,7 @@ import HotspotButton from "./HotspotButton";
 import React from "react";
 import hotspotConfig from "../../configs/hotspotConfig.json";
 
+
 function ModelViewer() {
     return (
         <model-viewer
