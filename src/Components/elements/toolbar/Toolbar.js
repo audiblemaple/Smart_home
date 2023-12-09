0c0dd4d0144cf5d2cea564efc9f4086c17400300
Lior <audiblemaple@gmail.com>
2023-12-09 20:24:01 +0200

    Added toolbar as a hideable menu.

--- src/Components/elements/toolbar/Toolbar.js
+++ src/Components/elements/toolbar/Toolbar.js
@@ -1,32 +1,55 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import "./toolbar_style.css";
 
 function Toolbar() {
     const navigate = useNavigate();
 
-    const defaultView = () => {
+    const [isClosed, setIsClosed] = useState(false);
+    const toolbarClass = isClosed ? 'toolbar_open' : 'toolbar_closed';
+    const toolbarRef = useRef();
+
+    const defaultView = (event) => {
         const modelViewer = document.getElementById('model');
         modelViewer.setAttribute('camera-target', "0m 0m 0m");
         modelViewer.setAttribute('camera-orbit', '0deg 0deg 28m');
-    }
+    };
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (toolbarRef.current && !toolbarRef.current.contains(event.target))
+                setIsClosed(true);
+        }
+
+        // Add the event listener
+        document.addEventListener("mousedown", handleClickOutside);
+
+        // Clean up
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
 
     const meshControlPanel = () => {
         navigate('/mesh');
     }
 
+    const toggleToolbar = () => {
+        setIsClosed(!isClosed);
+    };
+
     return (
-        <div className="toolbar">
-            <button
+        <div className={toolbarClass} onClick={toggleToolbar}>
+            <div
                 id="defaultViewButton"
                 className="toolbar-button"
                 onClick={defaultView}>
-            </button>
-            <button
+            </div>
+            <div
                 id="meshControlPanelButton"
                 className="toolbar-button"
                 onClick={meshControlPanel}>
-            </button>
+            </div>
         </div>
     );
 }
