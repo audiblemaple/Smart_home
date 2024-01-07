1a7fc74734db6f3defed3c78c36ddb438e37c916
Lior <audiblemaple@gmail.com>
2024-01-07 18:51:36 +0200

    optimizations...
    Signed-off-by: Lior <audiblemaple@gmail.com>

--- src/Components/elements/Popup/ErrorPopup.js
+++ src/Components/elements/Popup/ErrorPopup.js
@@ -10,7 +10,7 @@ function ErrorPopup({ message }) {
             setTimeout(() => {
                 console.log(showPopup);
                 setShowPopup(false);
-            }, 3000);
+            }, (Number(process.env.REACT_APP_ERROR_DISPLAY_DURATION) || 2) * 1000);
         }
     }, [message]);
 
